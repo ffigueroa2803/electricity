abd769e63610640a5604262124533f46c6f7d3c4
Frankie Amilcar Figueroa Muñoz <ffigueroa2803@gmail.com>
2023-06-08 18:50:11 -0500

    changed

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -10,22 +10,26 @@ export const PublicLayout = () => {
   const isLoggedIn = useAuth();
   const authChecked = useAuthCheck();
 
-  if (!authChecked) {
-    return <LoadingPage />;
-  }
-
-  if (isLoggedIn) {
-    return <Navigate to="/authorized/profile" />;
-  }
-
-  return (
-    <div className="flex w-full h-screen">
-      <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center">
-        <img width="765px" height="482px" src={LogoImage} className="" alt="" />
+  return authChecked ? (
+    !isLoggedIn ? (
+      <div className="flex w-full h-screen">
+        <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center">
+          <img
+            width="765px"
+            height="482px"
+            src={LogoImage}
+            className=""
+            alt=""
+          />
+        </div>
+        <div className="w-full flex items-center justify-center bg-gray-50 lg:w-1/2">
+          <Outlet />
+        </div>
       </div>
-      <div className="w-full flex items-center justify-center bg-gray-50 lg:w-1/2">
-        <Outlet />
-      </div>
-    </div>
+    ) : (
+      <Navigate to="/authorized/profile" />
+    )
+  ) : (
+    <LoadingPage />
   );
 };
