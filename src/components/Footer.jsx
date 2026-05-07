91a0394e38d5b11a24f83a66e48fc27d87dbeb1a
ffigueroa2803 <ffigueroa2803@gmail.com>
2026-05-07 09:39:02 -0500

    se modifico el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <p className="text-gray-700 text-center m-20 dark:text-white">
-      © 2025 Todos los derechos reservados por CyCFFigueroa sac.
+      © 2026 Todos los derechos reservados por cilia-yoshi.
     </p>
   );
 };
